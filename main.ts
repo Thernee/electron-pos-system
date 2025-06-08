cf1a0f558fc520b882b22967ecfde1dba8f0aad5
Sani Abubakar Adam <Athernee@gmail.com>
2025-06-08 08:39:06 +0100

    feat: implement desktop app electron entry

---
+++ main.ts
@@ -0,0 +1,32 @@
+// import { app, BrowserWindow } from 'electron';
+// import * as path from 'path';
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // load the react app. adjust path after setup
+  win.loadFile(path.join(__dirname, 'frontend/build/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
