9fc0752fa7286e62e2aada7b08fbfb44685594cc
Sani Abubakar Adam <Athernee@gmail.com>
2025-06-08 11:58:33 +0100

    feat: integrate nest, electron and react together

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
-// import { app, BrowserWindow } from 'electron';
-// import * as path from 'path';
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
+// const { app, BrowserWindow } = require("electron");
+// const path = require("path");
+
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './backend/src/app.module';
+
+
+async function bootstrap() {
+  const nestApp = await NestFactory.create(AppModule);
+  await nestApp.listen(3000)
+}
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -13,11 +22,15 @@ function createWindow() {
     },
   });
 
+  win.loadURL('http://localhost:3000');
   // load the react app. adjust path after setup
-  win.loadFile(path.join(__dirname, 'frontend/build/index.html'));
+  // win.loadFile(path.join(__dirname, 'frontend/build/index.html'));
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(async () => {
+  await bootstrap();
+  createWindow();
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
