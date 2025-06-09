29f039f395a70d2de6b7492c2564bc3d0c162f73
Sani Abubakar Adam <Athernee@gmail.com>
2025-06-09 16:27:02 +0100

    chore: unified all modules in appModule

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { CustomerController } from './customer/customer.controller';
+import { CustomerService } from './customer/customer.service';
+import { TransactionController } from './transaction/transaction.controller';
+import { TransactionService } from './transaction/transaction.service';
+import { CashWalletController } from './cash-wallet/cash-wallet.controller';
+import { CashWalletService } from './cash-wallet/cash-wallet.service';
+import { TransactionModule } from './transaction/transaction.module';
+import { CashWalletModule } from './cash-wallet/cash-wallet.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [TransactionModule, CashWalletModule, CustomerModule],
+  controllers: [AppController, CustomerController, TransactionController, CashWalletController],
+  providers: [AppService, PrismaService, CustomerService, TransactionService, CashWalletService],
 })
 export class AppModule { }
