27db5199bd2c5e30826c1c34ef682803c538c077
Sani Abubakar Adam <Athernee@gmail.com>
2025-06-10 08:38:23 +0100

    chore: removed redundant code

--- apps/electron/main.ts
+++ apps/electron/main.ts
@@ -19,32 +19,3 @@ function createWindow() {
 }
 
 app.whenReady().then(createWindow);
-
-
-
-// import { app, BrowserWindow } from 'electron';
-// import { NestFactory } from '@nestjs/core';
-
-// import { AppModule } from '../backend/dist/app.module.js';
-
-// async function bootstrap() {
-//   const nest = await NestFactory.create(AppModule);
-//   await nest.listen(3000);
-// }
-
-// function createWindow() {
-//   const win = new BrowserWindow({
-//     width: 800, height: 600,
-//     webPreferences: { nodeIntegration: true, contextIsolation: false }
-//   });
-//   win.loadURL('http://localhost:3000');
-// }
-
-// app.whenReady().then(async () => {
-//   await bootstrap();
-//   createWindow();
-//   app.on('activate', () => {
-//     if (BrowserWindow.getAllWindows().length === 0) createWindow();
-//   });
-// });
-// app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit(); });
