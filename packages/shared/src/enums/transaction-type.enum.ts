77c7e6cadea92f719136e885406556c93b876626
Sani Abubakar Adam <Athernee@gmail.com>
2025-06-15 18:05:57 +0100

    refactor: removed the packages dir

--- packages/shared/src/enums/transaction-type.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum TransactionType {
-  DEBIT = 'DEBIT',
-  CREDIT = 'CREDIT',
-  CASH = 'CASH',
-}
--- packages/shared/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './dtos/create-customer.dto';
-export * from './enums/transaction-type.enum';
